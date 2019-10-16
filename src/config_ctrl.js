21a3660ed144a683627e7ee468c58d99a905591e
Guranjan Singh <guranjan@Guranjans-MacBook-Pro-2.local>
2019-10-16 11:27:57 -0700

    Add datasource to source control.

---
+++ src/config_ctrl.js
@@ -0,0 +1,12 @@
+
+export class GenericConfigCtrl {
+  constructor($scope)  {
+    this.scope = $scope;
+    if (!this.current.jsonData.scalyrUrl) {
+      this.current.jsonData.scalyrUrl = 'https://www.scalyr.com/'
+    }
+  }
+}
+
+GenericConfigCtrl.templateUrl = 'partials/config.html';
+
---
+++ src/query_ctrl.js
@@ -0,0 +1,44 @@
+import {QueryCtrl} from 'grafana/app/plugins/sdk'
+
+export class GenericDatasourceQueryCtrl extends QueryCtrl {
+
+  constructor($scope, $injector)  {
+    super($scope, $injector);
+    this.scope = $scope;
+  }
+
+  getFacetFunctionOptions() {
+    let options = [];
+    if (!this.target.facet) {
+      options.push({
+        text: 'count', value: 'count'
+      })
+    }
+    options = options.concat(
+      [
+        { text: 'mean', value: 'mean'},
+        { text: 'min', value: 'min'},
+        { text: 'max', value: 'max'},
+        { text: 'sumPerSec', value: 'sumPerSec'},
+        { text: '10th %ile', value: 'p10'},
+        { text: '50th %ile', value: 'p50'},
+        { text: '90th %ile', value: 'p90'},
+        { text: '95th %ile', value: 'p95'},
+        { text: '99th %ile', value: 'p99'},
+        { text: '99.9th %ile', value: 'p999'}
+      ]
+    )
+    return options;
+  }
+
+  toggleEditorMode() {
+    this.target.rawQuery = !this.target.rawQuery;
+  }
+
+  onChangeInternal() {
+    this.panelCtrl.refresh(); // Asks the panel to refresh data.
+  }
+}
+
+GenericDatasourceQueryCtrl.templateUrl = 'partials/query.editor.html';
+
